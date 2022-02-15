7d4709ee239fa4a9b93a3de5036300aad695e5dc
Albin Shrestha <43053302+Albisourous@users.noreply.github.com>
2022-02-14 18:23:32 -0600

    version 1

--- Strike Sphere/Assets/BallControll.cs
+++ Strike Sphere/Assets/BallControll.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class BallControll : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float power = 10f;
+    public float maxDrag = 5f;
+    public Rigidbody2D rb;
+    public LineRenderer lr;
+
+    Vector3 dragStartPos;
+    Touch touch;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                DragStart();
+            }
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Dragging();
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                DragRelease();
+            }
+        }
+    }
+
+    void DragStart()
+    {
+        dragStartPos = Camera.main.ScreenToWorldPoint(touch.position);
+        dragStartPos.z = 0f;
+        lr.positionCount = 1;
+        lr.SetPosition(0, dragStartPos);
+    }
+
+    void Dragging()
+    {
+        Vector3 draggingPos = Camera.main.ScreenToWorldPoint(touch.position);
+        dragStartPos.z = 0f;
+        lr.positionCount = 2;
+        lr.SetPosition(1, draggingPos);
+    }
+
+    void DragRelease()
+    {
+        lr.positionCount = 0;
+
+        Vector3 dragReleasePos = Camera.main.ScreenToWorldPoint(touch.position);
+        dragReleasePos.z = 0f;
+
+        Vector3 force = dragStartPos - dragReleasePos;
+        Vector3 clampedForce = Vector3.ClampMagnitude(force, maxDrag * power);
+
+        rb.AddForce(clampedForce, ForceMode2D.Impulse);
     }
 }
